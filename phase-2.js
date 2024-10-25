22c279b9f76291a85c44ac1650d02381a6418c47
Tom Hennessy <tomlhennessy@outlook.com>
2024-10-25 11:41:54 +1000

    refactoring with time checks, allowing for error handling

--- phase-2.js
+++ phase-2.js
@@ -3,7 +3,7 @@ function stretch(timeLeft) {
     if (timeLeft < 1000) {
 
       // if we dont have enough time to complete the action
-      // reject the promise with the reason 
+      // reject the promise with the reason
       reject('you dont have enough time to stretch')
 
     } else {
@@ -57,12 +57,15 @@ function liftWeights(timeLeft) {
 
 // refactor this function to handle Promises using async/await instead of
   // .then and .catch
-function workout(totalTime) {
-  stretch(totalTime)
-    .then(timeLeftAfterStretching => runOnTreadmill(timeLeftAfterStretching))
-    .then(timeLeftAfterRunning => liftWeights(timeLeftAfterRunning))
-    .then(res => console.log(`done working out with ${res/1000} seconds left`))
-    .catch(err => console.log('Error: ', err));
+async function workout(totalTime) {
+  try {
+    let timeLeftAfterStretching = await stretch(totalTime);
+    let timeLeftAfterRunning = await runOnTreadmill(timeLeftAfterStretching);
+    let timeLeftAfterLifting = await liftWeights(timeLeftAfterRunning);
+    console.log(`done working out with ${timeLeftAfterLifting / 1000} seconds left`);
+  } catch (err) {
+    console.log('Error:', err);     // handle errors if there's not enough time
+  }
 }
 
 
@@ -73,25 +76,25 @@ Comment in each invocation of your workout function below and run the file
 */
 
 
-// workout(500);
+workout(500);
   // should print out the following:
     // Error:  you dont have enough time to stretch
 
 
-// workout(1000);
+workout(1000);
   // should print out the following:
     // done stretching
     // Error:  you dont have enough time to run on treadmill
 
 
-// workout(2000);
+workout(2000);
   // should print out the following:
     // done stretching
     // done running on treadmill
     // Error:  you dont have enough time to lift weights
 
 
-// workout(4000);
+workout(4000);
   // should print out the following:
   //   done stretching
   //   done running on treadmill
